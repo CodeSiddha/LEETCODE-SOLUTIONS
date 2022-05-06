8cb98f5041e7e13c6684fa72fcc819f339557e8b
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-05-06 18:59:42 +0530

    Time: 10 ms (91.27%), Space: 15.8 MB (53.22%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,16 +2,40 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int , int> mpp;
-        for(int i = 0 ; i < n ;i++){
-            mpp[nums[i]]++;
+        int num1 = -1 , num2 = -1 , c1 = 0 , c2 = 0 , i;
+        for( i = 0 ; i < n ; i++){
+            if(num1 == nums[i]){
+                c1++;
+            }else if(num2 == nums[i]){
+                c2++;
+            }else if(c1 == 0){
+                num1 = nums[i];
+                c1 = 1;
+            }else if(c2 == 0){
+                num2 = nums[i];
+                c2 = 1; 
+            }else{ 
+                c1--;
+                c2--;
+            } 
         }
-        vector<int> ans;
-        for(auto &x : mpp){
-            if(x.second > n/3){
-                ans.push_back(x.first);
+        // cout << num1 << " " << num2;
+        int count1 = 0 , count2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
             }
         }
-        return ans;
+        if((count1 > n/3) && (count2 > n/3)){
+            return {num1 , num2};
+        }else if(count1 > n/3){
+            return {num1};
+        }else if(count2 > n/3){
+            return {num2};
+        }else{
+            return {};
+        }
     }
 };
