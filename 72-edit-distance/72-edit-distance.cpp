7cb6ac529309a49bfdec0f78060d3a64681cccdb
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-06-13 22:58:50 +0530

    Time: 19 ms (54.99%), Space: 6.4 MB (97.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,22 +4,29 @@ public:
     int minDistance(string w1, string w2) {
         int n = w1.size();
         int m = w2.size();
-        vector<vector<int>>dp(n + 1 , vector<int>(m + 1 , 0));
-        for(int i = 1 ; i < n + 1 ; i++){
-            dp[i][0] = i;
-        }
+        // vector<vector<int>>dp(n + 1 , vector<int>(m + 1 , 0));
+        vector<int> prev(m + 1 , 0) , curr(m + 1 , 0);
+        // for(int i = 1 ; i < n + 1 ; i++){
+        //     dp[i][0] = i;
+        // }
         for(int j = 1 ; j < m + 1 ; j++){
-            dp[0][j] = j;
+            // dp[0][j] = j;
+            prev[j] = j; // For the first row. all values
         }
         for(int i = 1 ; i < n + 1 ; i++){
+            curr[0] = i; // val of first col in every iteration.
             for(int j = 1 ; j < m + 1 ; j++){
                 if(w1[i - 1] == w2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    // dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 }else{
-                    dp[i][j] = 1 + min(dp[i - 1][j] , min(dp[i][j - 1] , dp[i - 1][j - 1]));
+                    // dp[i][j] = 1 + min(dp[i - 1][j] , min(dp[i][j - 1] , dp[i - 1][j - 1]));
+                    curr[j] = 1 + min(prev[j] , min(curr[j - 1] , prev[j - 1]));
                 }
             }
+            prev = curr;
+            
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
