a15aaba8b0fd278affb8e8266abce34f2cd36e36
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-06-15 00:03:30 +0530

    Time: 4 ms (75.10%), Space: 9.2 MB (78.58%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -5,13 +5,13 @@ public:
         return false;
     }
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin() , strs.end() , cmp);
+        // sort(strs.begin() , strs.end() , cmp);
         int n = strs.size();
         string lcp = strs[0];
         for(int i = 1 ; i < n ; i++){
             if(lcp == "") break;
             int p = 0;
-            while(p < lcp.size() && lcp[p] == strs[i][p]){
+            while(p < lcp.size() && p < strs[i].size() && lcp[p] == strs[i][p]){
                 p++;
             }
             lcp = lcp.substr(0 , p);
