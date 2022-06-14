c5557a45fdf7ca0f7626bb66dd57a3d62ab2aa89
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-06-14 19:46:18 +0530

    Time: 14 ms (84.46%), Space: 7 MB (93.53%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,7 +3,8 @@ public:
     int minDistance(string w1, string w2) {
         int n = w1.size();
         int m = w2.size();
-        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+        // vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+        vector<int> prev(m + 1 , 0) , curr(m + 1 , 0); 
         // for(int i = 1; i < n + 1 ; i++){
         //     dp[i][0] = i;
         // }
@@ -11,14 +12,16 @@ public:
         //     dp[0][j] = j;
         // }
         for(int i = 1 ; i < n + 1 ; i++){
-            for(int j = 1 ;j < m + 1; j++){
+            // curr[0] = 0;
+            for(int j = 1 ; j < m + 1; j++){
                 if(w1[i - 1] == w2[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }else{
-                    dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]);
+                    curr[j] = max(prev[j] , curr[j - 1]);
                 }
             }
+            prev = curr;
         }
-        return ((n + m) - 2*dp[n][m]);
+        return ((n + m) - 2*prev[m]);
     }
 };
