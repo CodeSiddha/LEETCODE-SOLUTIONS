c93c4afdfd248d5a4dca7045bf126278fc0c7adb
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-06-15 15:08:36 +0530

    Time: 788 ms (6.17%), Space: 26.9 MB (22.15%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,6 +1,38 @@
 class Solution {
+private:
+    vector<unordered_map<char,int>> freq;
 public:
-   
+    int dp[1001][1001];
+    int solve(int i , int n , vector<string>&arr , int pred){
+        if(i == n){
+            return 0;
+        }
+        if(dp[i][pred] != -1){
+            return dp[i][pred];
+        }
+        if(i == pred){
+            return dp[i][pred] = max(1 + solve(i + 1 , n , arr , i) , solve(i + 1 , n , arr , i + 1));
+        }else{
+            if(arr[pred].size() + 1 == arr[i].size()){
+                string s = arr[i];
+                bool found = false;
+                for(int j = 0 ; j < (int)s.size() ; j++){
+                    string pres = s.substr(0 , j) + s.substr(j + 1);
+                    if(pres == arr[pred]){
+                        found = true;
+                        break;
+                    }
+                }
+                if(found){
+                    return dp[i][pred] = max(1 + solve(i + 1 , n , arr , i) , solve(i + 1 , n , arr , pred));
+                }else{
+                    return dp[i][pred] = solve(i + 1 , n , arr , pred);
+                }
+            }else{
+                return dp[i][pred] = solve(i + 1 , n , arr , pred);
+            }
+        }
+    }
     static bool cmp(string &a , string&b){
         if(a.size() < b.size()) return true;
         return false;
@@ -8,19 +40,15 @@ public:
     int longestStrChain(vector<string>& words) {
         int n = words.size();
         sort(words.begin() , words.end() , cmp);
-        unordered_map<string , int> prechain;
-        int ans = 0;
-        for(auto str : words){
-            for(int i = 0 ; i < str.size() ; i++){
-                string pre = str.substr(0 , i) + str.substr(i + 1);
-                if(prechain.count(pre)){
-                    prechain[str] = max(prechain[str] , prechain[pre] + 1);
-                }else{
-                    prechain[str] = max(1 , prechain[str]);
-                }
-            }
-            ans = max(ans , prechain[str]);
-        }
-        return ans;
+        // for(int i = 0 ; i < n ; i++){
+        //     string str = words[i];
+        //     unordered_map<char , int> Sfreq;
+        //     for(int j = 0 ; j < (int) str.size() ; j++){
+        //         Sfreq[str[j]] = j;
+        //     }
+        //     freq.push_back(Sfreq);
+        // }
+        memset(dp , -1 , sizeof(dp));
+        return solve(0 , n , words , 0 );
     }
 };
