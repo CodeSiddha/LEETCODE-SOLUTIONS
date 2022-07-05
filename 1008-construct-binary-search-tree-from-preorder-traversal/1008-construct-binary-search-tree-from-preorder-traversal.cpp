1889f5e86b4a23044d3e785f91ccb271b21c251e
Vishal  Siddha <33022181+siddhakiller@users.noreply.github.com>
2022-07-05 18:31:24 +0530

    Time: 8 ms (56.21%), Space: 13.8 MB (47.50%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,32 +11,16 @@
  */
 class Solution {
 public:
-    int search(vector<int>&preorder , int preStart , int preEnd , int val){
-        int i;
-        for(i = preStart ;i <= preEnd ; i++){
-            if(val < preorder[i]){
-                break;
-            }
-        }
-        return i - 1;
-    }
-    int pre = 0;
-    TreeNode* MakeBST(vector<int>&preorder , int preStart , int preEnd){
-        if(preStart > preEnd){
-            return NULL;
-        }
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        if(preStart == preEnd){
-            return root;
-        }
-        int Insearch = search(preorder , preStart + 1 , preEnd , root->val);
-        root->left = MakeBST(preorder , preStart + 1 , Insearch);
-        root->right = MakeBST(preorder , Insearch + 1 , preEnd);
+    TreeNode* MakeBST(vector<int>&preorder , int &i , int bound){
+        if(i == preorder.size() || preorder[i] > bound) return NULL;
+        TreeNode* root = new TreeNode(preorder[i++]);
+        root->left = MakeBST(preorder , i , root->val);
+        root->right = MakeBST(preorder , i , bound);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int n = preorder.size();
-        TreeNode* root = MakeBST(preorder , 0 , n - 1);
+        int i = 0;
+        TreeNode* root = MakeBST(preorder , i , INT_MAX);
         return root;
     }
 };
